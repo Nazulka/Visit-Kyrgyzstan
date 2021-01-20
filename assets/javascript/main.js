5020f962d613d9714e803e7bfbdbdc7b192738e5
Nazulka <nazeekadiu@yahoo.co.uk>
2021-01-20 20:29:27 +0000

    work on javascript read more read less button

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,15 +1,28 @@
-var i=0;
-function read() {
-  if(!i) {
-      document.getElementById("more").style.display = "inline";
-      document.getElementById("dots").style.display = "none";
-      document.getElementById("read").innerHTML = "Read less";
-      i=1;
-  }
-  else {
-      document.getElementById("more").style.display = "none";
-      document.getElementById("dots").style.display = "inline";
-      document.getElementById("read").innerHTML = "Read more";
-      i=0;
-  }
-}
+// function myFunction() {
+//   var dots = document.getElementById("dots-1");
+//   var moreText = document.getElementById("more-1");
+//   var btnText = document.getElementById("read-1");
+
+//   if (dots.style.display === "none") {
+//     dots.style.display = "inline";
+//     btnText.innerHTML = "Read more";
+//     moreText.style.display = "none";
+//   } else {
+//     dots.style.display = "none";
+//     btnText.innerHTML = "Read less";
+//     moreText.style.display = "inline";
+//   }
+// }
+
+$(document).ready(function(){
+    $("button.read").click(function(){
+        $(this).prev().toggle();
+        $(this).siblings(".dots").toggle();
+        if($(this).text()=="read more"){
+            $(this).text("read less");
+        }
+        else{
+            $(this).text("read more");
+        }
+    });
+});
