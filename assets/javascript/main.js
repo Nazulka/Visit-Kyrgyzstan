db560ee4ff8c5a89c077c50e07791021d13f3fe7
Nazulka <nazeekadiu@yahoo.co.uk>
2021-01-21 13:20:40 +0000

    fix read more read less buttons jQuery

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -14,15 +14,33 @@
 //   }
 // }
 
-$(document).ready(function(){
-    $("button.read").click(function(){
-        $(this).prev().toggle();
-        $(this).siblings(".dots").toggle();
-        if($(this).text()=="read more"){
-            $(this).text("read less");
-        }
-        else{
-            $(this).text("read more");
-        }
-    });
+// $(document).ready(function(){
+//     $(".read").click(function(){
+//         $(this).prev().toggle();
+//         $(this).siblings('.dots').toggle();
+//         if($(this).text()=='read more'){
+//             $(this).text('read less');
+//         }
+//         else{
+//             $(this).text('read more');
+//         }
+//     });
+// });
+
+var cards = document.querySelector('#explore-cards');
+
+cards.addEventListener('click', function(e) {
+  var target = e.target;
+  if (target.classList.contains('btn')) {
+    var cardBody = target.parentNode;
+    var open = target.dataset.open === 'true';
+    target.dataset.open = !open;
+    if (open) {
+      target.innerText = 'Read more';
+      cardBody.classList.remove('open');
+    } else {
+      target.innerText = 'Read less';
+      cardBody.classList.add('open');
+    }
+  }
 });
