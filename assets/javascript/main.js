67ee21e8000f881db922ff2cde4ccf5918b99d3f
Nazulka <nazeekadiu@yahoo.co.uk>
2021-01-21 21:32:16 +0000

    start map section html

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,32 +1,3 @@
-// function myFunction() {
-//   var dots = document.getElementById("dots-1");
-//   var moreText = document.getElementById("more-1");
-//   var btnText = document.getElementById("read-1");
-
-//   if (dots.style.display === "none") {
-//     dots.style.display = "inline";
-//     btnText.innerHTML = "Read more";
-//     moreText.style.display = "none";
-//   } else {
-//     dots.style.display = "none";
-//     btnText.innerHTML = "Read less";
-//     moreText.style.display = "inline";
-//   }
-// }
-
-// $(document).ready(function(){
-//     $(".read").click(function(){
-//         $(this).prev().toggle();
-//         $(this).siblings('.dots').toggle();
-//         if($(this).text()=='read more'){
-//             $(this).text('read less');
-//         }
-//         else{
-//             $(this).text('read more');
-//         }
-//     });
-// });
-
 var cards = document.querySelector('#explore-cards');
 
 cards.addEventListener('click', function(e) {
